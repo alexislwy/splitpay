2ead46d9306aa5e1cfdd8660f405799c20b1161d
alexislwy <alexislwy@gmail.com>
2026-05-19 08:00:59 +0800

    Create firebaseConfig.js

---
+++ src/firebaseConfig.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCaUWzi9LqK7RA5-imPV8VGxPtSoAU1ATE",
+  authDomain: "splitpay-2dd70.firebaseapp.com",
+  databaseURL: "https://splitpay-2dd70-default-rtdb.firebaseio.com",
+  projectId: "splitpay-2dd70",
+  storageBucket: "splitpay-2dd70.firebasestorage.app",
+  messagingSenderId: "24823461536",
+  appId: "1:24823461536:web:dd01804b0f9c47248cbbc5"
+};
+
+export default firebaseConfig;
